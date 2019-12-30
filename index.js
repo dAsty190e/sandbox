8d19bd8157c7736110b807117db67077ca655994
unknown <m.lesya11@gmail.com>
2019-12-30 17:04:40 +0200

    my first program

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = express() // () means execute
 // set the port to 3000 (localhost:3000)
 const port = 3000
 
+var cat = "Basya says MEOOOOOOWWWWWWW";
+
 // create a path '/' and respond with content
-app.get('/', (req, res) => res.send('Hello World! yey!'))
+app.get('/', (req, res) => res.send('Hello World! yey! ' + cat))
 app.get('/test', (req, res) => res.send('a test page'))
 
 // setup a listener and when ready print out a message
---
+++ variables2.js
@@ -0,0 +1,7 @@
+var cat = "basya";
+var dog = "lucky";
+var ferret = "Theogirl";
+//the quick brown fox jumped over the lazy dog
+console.log(cat);
+console.log(ferret);
+console.log(dog);
