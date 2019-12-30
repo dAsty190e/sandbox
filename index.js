9c252c1ffad2a69e9c31b6ca27b4480ac3692f66
unknown <m.lesya11@gmail.com>
2019-12-30 16:34:47 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// bring in express.js dependencies
+const express = require('express')
+// start express.js
+const app = express() // () means execute
+// set the port to 3000 (localhost:3000)
+const port = 3000
+
+// create a path '/' and respond with content
+app.get('/', (req, res) => res.send('Hello World! yey!'))
+app.get('/test', (req, res) => res.send('a test page'))
+
+// setup a listener and when ready print out a message
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
