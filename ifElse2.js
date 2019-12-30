e3f3abb397bc8e45db13e0a610914f95fa0f829d
unknown <m.lesya11@gmail.com>
2019-12-30 20:32:07 +0200

    extra comment

--- ifElse2.js
+++ ifElse2.js
@@ -12,6 +12,7 @@ var question = 'hello, welcome to the DMV. You need to be 18 or older for a driv
 
 // readline.question is used to ask a question
 // readline.question([the question], ([the response]) => { do something with the response })
+// the () => {} is called a callback function...
 realine.question(question, (response) => {
     if (response >= 18) {
         console.log(`Thank you, here is your drivers lisence`);        
