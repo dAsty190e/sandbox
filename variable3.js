6c0c29ae2c07f8f5bdc7226180ca8315c0705d0c
unknown <m.lesya11@gmail.com>
2019-12-30 17:25:23 +0200

    varisble3.js

--- variable3.js
+++ variable3.js
@@ -2,3 +2,19 @@ var fish = "Lina";
 var dog = "Lup";
 
 console.log (fish+dog);
+
+var elephant = "niki";
+var messadge = "good morning1"
+
+console.log(elephant + " says " + messadge + " to " + fish)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
+console.log(elephant + " says " + messadge + " to " + dog)
